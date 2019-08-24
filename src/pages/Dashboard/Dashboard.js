4c8e792874c01cf032cac98ce669a39d303fbf80
fshlarf <faishal.arif218@gmail.com>
2019-08-24 09:48:36 +0700

    add Dashboard page, delete useless files

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return  (
+      <div>Hello from dashboard</div>
+    )
+  }
+}
+
+export default Dashboard
